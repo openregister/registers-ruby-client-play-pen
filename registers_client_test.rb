65ffc842430f88c886a17a6c98eca3b99150e967
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-12-04 18:03:32 +0000

    add get_records_with_history example

--- registers_client_test.rb
+++ registers_client_test.rb
@@ -3,3 +3,6 @@ require 'registers_client'
 registers_client = RegistersClient::RegistersClientManager.new({ cache_duration: 3600 })
 register_data = registers_client.get_register 'country', 'beta'
 puts register_data.get_records.first[:item]
+
+germany = register_data.get_records_with_history.select { |r|  r[:key] == 'DE'  }
+puts germany.to_json

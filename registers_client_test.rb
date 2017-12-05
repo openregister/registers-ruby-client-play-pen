bed8e3ade530006ea3532e9a29d3363a3a3ac46e
Gideon Goldberg <gidsgoldberg@gmail.com>
2017-12-05 10:39:42 +0000

    use find instead of select

--- registers_client_test.rb
+++ registers_client_test.rb
@@ -4,5 +4,5 @@ registers_client = RegistersClient::RegistersClientManager.new({ cache_duration:
 register_data = registers_client.get_register 'country', 'beta'
 puts register_data.get_records.first[:item]
 
-germany = register_data.get_records_with_history.select { |r|  r[:key] == 'DE'  }
+germany = register_data.get_records_with_history.find { |r|  r[:key] == 'DE'  }
 puts germany.to_json
